4639c89def0ea8c500b1d5d71c434e1303ce7aae
Eloiza <eloiza.rossetto@gmail.com>
2023-01-22 20:54:09 -0300

    feat: add game window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <ncurses.h>
 
+#define WALL '_'
+
+void print_map(){
+    int y;
+    attron(COLOR_PAIR(2));
+    box(stdscr, 0, 0);
+
+    // for (y = 0; y < LINES; y++){
+    //         // mvhline(y, 0, WALL, 5);
+    // }
+    attroff(COLOR_PAIR(2));
+};
+
 int main(int argc, char **argv)
 {
     initscr();
@@ -7,13 +20,20 @@ int main(int argc, char **argv)
     noecho();
     nodelay(stdscr, TRUE);
     curs_set(FALSE); //hide cursor
+    resizeterm(30, 30);
 
     start_color(); 
     wattron(stdscr, A_BOLD);
-    init_pair(1, COLOR_YELLOW, COLOR_YELLOW);
+
+    init_pair(1, COLOR_YELLOW, COLOR_YELLOW); //pacman-color
+    // init_pair(2, COLOR_BLACK, COLOR_BLUE); //maze-color
+    init_pair(2, COLOR_BLUE, COLOR_BLACK); // maze-color
 
     char pacman = 'C';
-    int ch =0, x =0, y = 0;
+    int ch =0, x =0, y = 3;
+
+    print_map();
+    refresh();
 
     while ((ch = getch()) != 'q'){
         switch (ch){
@@ -31,11 +51,13 @@ int main(int argc, char **argv)
             break;
         }
         erase();
+        print_map();
+
         attron(COLOR_PAIR(1));
         mvaddch(y, x, pacman);
         attroff(COLOR_PAIR(1));
         move(y, x);
-        refresh();
+        // refresh();
     }
 
     endwin();
