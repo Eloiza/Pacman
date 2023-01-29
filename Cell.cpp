7c7d7b393d2d54c83c8852b2cf703d29bb5d0e0c
Eloiza <eloiza.rossetto@gmail.com>
2023-01-29 15:21:31 -0300

    feat: add cell class

---
+++ Cell.cpp
@@ -0,0 +1,6 @@
+#include "Cell.hpp"
+
+void Cell::setCell(unsigned int row, unsigned int col){
+    this->row = row;
+    this->col = col;
+};
---
+++ Cell.hpp
@@ -0,0 +1,11 @@
+#ifndef CELL_HPP
+#define CELL_HPP
+
+class Cell{
+    public:
+        void setCell(unsigned int row, unsigned int col);
+        unsigned int row; 
+        unsigned int col; 
+};
+
+#endif
