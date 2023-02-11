0dbdb06ee45c5d2678d9e5d79833c290e8c2e0f2
Eloiza <eloiza.rossetto@gmail.com>
2023-02-11 01:09:07 -0300

    chore: clean main code

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "Map.hpp"
 #include "Ghost.hpp"
 
-#include "ScatterBehavior.hpp"
+#include "Scatter.hpp"
 
 #include "ChaseAggresive.hpp"
 #include "ChaseAmbush.hpp"
@@ -17,49 +17,10 @@
 
 using namespace gameColors;
 
-// void printMap(Map * gameMap){
-//     unsigned int i, j;
-//     for (i = 0; i < gameMap->rows; i++){
-//         for (j = 0; j < gameMap->cols; j++)
-//         {
-//             std::cout << gameMap->map[i * gameMap->cols + j];
-//         }
-//         std::cout << std::endl;
-//     }
-// }
-
-// void move_ghost(Map * map, Ghost g, char c){
-//     map->map[g.previousPosition.x * (unsigned int)map->cols + g.previousPosition.y] = ' ';
-//     map->map[g.position.x * (unsigned int)map->cols +g.position.y] = c;
-// }
-
 int main(int argc, char **argv){
     /*init game map*/
     Map map;
     map.loadGameMap("./maps/map.txt");
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-
-    // RandomBehavior random_bh = RandomBehavior(&map, &ghost_position);
-    // g.setBehavior(&random_bh);
-    // std::cout << "Setted Random Behavior" << std::endl;
-
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-
-    // AmbushBehavior ambush_bh = AmbushBehavior(&map, &ghost_position, &pacman_position, 4);
-    // g.setBehavior(&ambush_bh);
-    // std::cout << "Setted Ambush Behavior" << std::endl;
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
-    // g.move();
-    // std::cout << g.getPosition()->row << "," << g.getPosition()->col << std::endl;
 
     /*init ncurses and console*/
     ConsoleDraw console = ConsoleDraw();
@@ -75,16 +36,11 @@ int main(int argc, char **argv){
     Cell corner_positionA = Cell(23, 2);
     Cell corner_positionB = Cell(21, 9);
 
-    ChaseRandom pokey_bh = ChaseRandom(&map, &ghost_position, pacman.getPosition());
-    Ghost g1 = Ghost(11, 15, &pokey_bh);
+    // ChaseRandom pokey_bh = ChaseRandom(&map, &ghost_position, pacman.getPosition());
+    // Ghost g1 = Ghost(11, 15, &pokey_bh);
 
-    // Cell ghost_position2 = Cell(11, 15);
-
-    // InkyBehavior inky_bh = InkyBehavior(&map, &ghost_position, pacman.getPosition(), &ghost_position2);
-    // Ghost g1 = Ghost(11, 15, &inky_bh);
-
-    // AmbushBehavior ambush_bh = AmbushBehavior(&map, &ghost_position2, pacman.getPosition(), 4);
-    // Ghost g2 = Ghost(11, 14, &ambush_bh);
+    Scatter scatter_bh = Scatter(&map, &ghost_position, &corner_positionA, &corner_positionB);
+    Ghost g1 = Ghost(11, 15, &scatter_bh);
 
      unsigned char ch = ' ';
      unsigned int count = 0;
@@ -96,20 +52,11 @@ int main(int argc, char **argv){
          pacman.move(ch);
          console.drawCharacter(pacman);
 
-         if (update_ghost == 30200)
-         {
+         if (update_ghost == 30200){
              g1.move();
-            //  g2.move();
-            //  g3.move();
-            //  g4.move();
-             // napms(90);
              update_ghost = 0;
          }
          console.drawCharacter(g1);
-        //  console.drawCharacter(g2);
-        //  console.drawCharacter(g3);
-        //  console.drawCharacter(g4);
-
          update_ghost++;
          update_path++;
          count++;
